3395955b017dd8e454f1e7dd84ef14653b87715f
cocaroach23 <cocaroach23@gmail.com>
2024-02-26 12:48:36 +0300

    добавлена папка с меню ресторана

---
+++ src/components/Restoran/index.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react"
+
+const Restoran = () => {
+
+    const [ items , setItems ] = useState({})
+
+        useEffect(() => {
+            fetch("https://www.bit-by-bit.ru/api/student-projects/restaurants/[SLUG_РЕСТОРАНА]")
+
+            fetch("https://www.bit-by-bit.ru/api/student-projects/restaurants/[SLUG_РЕСТОРАНА]/items")
+            .then((response) => response.json())
+            .then((data) => setItems(data))
+        }, [slug])
+
+     return(
+         
+         <div>
+            {restoran.map((items) => [
+                <div key={items.id}>
+                    <div>{items.restaurantId}</div>
+                    <h2>{items.name}</h2>
+                    <img 
+                        src={items.image} 
+                    />
+                    <p>{items.description}</p>
+                    <p>{items.price}</p>
+                    <button></button>
+                </div>
+                
+            ])}
+         </div>
+    )
+
+    
+}
+
+
+export default Restoran
