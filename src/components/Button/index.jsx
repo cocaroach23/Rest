f385de4c51dac5d6f788da4eef1cc9928b60b9ba
cocaroach23 <cocaroach23@gmail.com>
2024-03-01 14:07:49 +0300

    не получается вывести корзину в модальное окно

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,10 @@
 import '../Button/index.css'
 
-function Button(props) {
+function Button(props, handleClick) {
 
     const { title, onClick } = props
     return(
-        <button onClick={onClick} className="style-button mx-10 rounded-lg text-xxl text-black px-4 py-2 shadow-md">
+        <button onClick={onClick} handleClick={(event) => handleClick(event)} className="style-button mx-10 rounded-lg text-xxl text-black px-4 py-2 shadow-md">
             {title}
         </button>
     )
